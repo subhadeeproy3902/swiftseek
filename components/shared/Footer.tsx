2f8f6871fd0d8f03ff9fd3bab184ecc2691acbc6
ParnaRoyChowdhury777 <subha9.5roy350@gmail.com>
2025-01-20 00:04:42 +0530

    Add z-index to Footer container for improved layering

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      <div className="container flex min-h-[72px] items-center justify-between border-t border-[#D2D2D2] px-4 pb-3 pt-5 lg:min-h-[72px] lg:px-0 lg:py-5">
+      <div className="container z-50 flex min-h-[72px] items-center justify-between border-t border-[#D2D2D2] px-4 pb-3 pt-5 lg:min-h-[72px] lg:px-0 lg:py-5">
         <a href="/" className="flex items-center gap-2.5">
           <Image
             unoptimized
